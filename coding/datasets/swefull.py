696d7b89175d8dda0339afeae314be8a35742178
brokespace <brokespace@nil.com>
2025-02-18 17:51:54 -0700

    changed swefull dataset to test split

--- coding/datasets/swefull.py
+++ coding/datasets/swefull.py
@@ -1,22 +1,4 @@
-# The MIT License (MIT)
-# Copyright © 2024 Yuma Rao
-# Copyright © 2023 Opentensor Foundation
-# Copyright © 2024 Macrocosmos
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
-# documentation files (the “Software”), to deal in the Software without restriction, including without limitation
-# the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
-# and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all copies or substantial portions of
-# the Software.
-
-# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
-# THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-# DEALINGS IN THE SOFTWARE.
-
+import tempfile
 from datasets import load_dataset
 
 from .base import Dataset
@@ -30,7 +12,7 @@ class SWEFullDataset(Dataset):
         self,
     ):
         # load in princeton-nlp/SWE-bench and shuffle the dataset
-        self.dataset = load_dataset("princeton-nlp/SWE-bench", split="train", streaming=True).shuffle()
+        self.dataset = load_dataset("princeton-nlp/SWE-bench", split="test", streaming=True).shuffle()
         self.dataset_iterset = iter(self.dataset)
 
     def get(self, n=100, selector: Selector = Selector()) -> dict:
@@ -39,7 +21,7 @@ class SWEFullDataset(Dataset):
             "topic": row["problem_statement"],
             "title": row["repo"],
             "content": row["patch"],
-            "extras": dict(pull_number="", base_commit=row["base_commit"]),
+            "extras": dict(pull_number="", base_commit=row["base_commit"], row=row),
         }
 
     def search(self, query, selector: Selector = None, **kwargs):
