8dbd067fd1a26f80e110b790f2b91e72c54c52da
brokespace <brokespace@nil.com>
2025-02-28 08:56:16 -0700

    added os import

--- coding/helpers/containers.py
+++ coding/helpers/containers.py
@@ -1,3 +1,4 @@
+import os
 import io
 import docker
 import logging
@@ -379,17 +380,22 @@ if __name__ == "__main__":
 
 
 def test_docker_server():
-    docker_server = DockerServer(remote_host_url=os.getenv("REMOTE_DOCKER_HOST"), remote_host_registry=f"{os.getenv('DOCKER_HOST_IP')}:5000")
-    docker_server._local_client.images.pull("nginx")
-    docker_server.remote.run(image="nginx", name="nginx-test")
-    # stop the container
-    docker_server._remote_client.containers.get("nginx-test").stop()
-    # remove the container
-    docker_server._remote_client.containers.get("nginx-test").remove()
-    # remove the image
-    # Force remove the image since it may be referenced in multiple repositories
     try:
-        docker_server._remote_client.images.get("nginx").remove(force=True)
-        print("Successfully removed nginx image from remote client")
+        docker_server = DockerServer(remote_host_url=os.getenv("REMOTE_DOCKER_HOST"), remote_host_registry=f"{os.getenv('DOCKER_HOST_IP')}:5000")
+        docker_server._local_client.images.pull("nginx")
+        docker_server.remote.run(image="nginx", name="nginx-test")
+        # stop the container
+        docker_server._remote_client.containers.get("nginx-test").stop()
+        # remove the container
+        docker_server._remote_client.containers.get("nginx-test").remove()
+        # remove the image
+        # Force remove the image since it may be referenced in multiple repositories
+        try:
+            docker_server._remote_client.images.get("nginx").remove(force=True)
+            print("Successfully removed nginx image from remote client")
+        except Exception as e:
+            print(f"Error removing image: {e}")
+        return True
     except Exception as e:
-        print(f"Error removing image: {e}")
+        print(f"Error: {e}")
+        return False
