f2f5f99c247b8b8cf2bf7791804b19be81ccdaec
brokespace <brokespace@nil.com>
2025-03-10 12:29:51 -0600

    recreate dendrite just in case the passed in dendrite is broken

--- coding/finetune/keys.py
+++ coding/finetune/keys.py
@@ -1,3 +1,5 @@
+import bittensor as bt
+
 from coding.protocol import ProvisionKeySynapse
 from coding.utils.uids import get_uid_from_hotkey
 from coding.finetune.tracker import run_async_in_thread
@@ -6,13 +8,20 @@ class APIKey:
     def __init__(self, hotkey: str, validator):
         self.hotkey = hotkey
         self.validator = validator
+        self.wallet = validator.wallet
         self.key_synapse = ProvisionKeySynapse()
-    
+
     @property
     def key(self):
+        dendrite = bt.dendrite(wallet=self.wallet)
         uid = get_uid_from_hotkey(self.validator, self.hotkey)
         responses = run_async_in_thread(
-            self.validator.dendrite.aquery(axons=[self.validator.metagraph.axons[uid]], synapse=ProvisionKeySynapse(action="create"), timeout=20, deserialize=False)
+            dendrite.aquery(
+                axons=[self.validator.metagraph.axons[uid]],
+                synapse=ProvisionKeySynapse(action="create"),
+                timeout=20,
+                deserialize=False,
+            )
         )
         if len(responses) == 0:
             return None
@@ -20,8 +29,18 @@ class APIKey:
             raise Exception(f"Expected 1 response, got {len(responses)}")
         self.key_synapse = responses[0]
         return self.key_synapse.api_key
-    
-    def __delete__(self):
+
+    def delete(self):
+        dendrite = bt.dendrite(wallet=self.wallet)
         uid = get_uid_from_hotkey(self.validator, self.hotkey)
-        self.validator.dendrite.aquery(axons=[self.validator.metagraph.axons[uid]], synapse=ProvisionKeySynapse(action="delete", key_hash=self.key_synapse.key_hash), timeout=20, deserialize=False)
-    
+        responses = run_async_in_thread(
+            dendrite.aquery(
+                axons=[self.validator.metagraph.axons[uid]],
+                synapse=ProvisionKeySynapse(
+                    action="delete", key_hash=self.key_synapse.key_hash
+                ),
+                timeout=20,
+                deserialize=False,
+            )
+        )
+        print(responses)
